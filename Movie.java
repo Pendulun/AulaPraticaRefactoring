852f48bc556b58ca51a3c218744302d72bb95213
Daniel Campos <danielcampos14@hotmail.com.br>
2021-08-04 14:43:19 +0000

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,19 +5,15 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price ;
 
   public Movie(String title, int priceCode) {
       _title = title;
-      _priceCode = priceCode;
+      this.setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
-  }
-
-  public void setPriceCode(int arg) {
-      _priceCode = arg;
+      return this._price.getPriceCode();
   }
 
   public String getTitle (){
@@ -51,4 +47,20 @@ public class Movie {
       frequentRenterPoints ++;
     return frequentRenterPoints;
   }
+
+  public void setPriceCode(int arg) {
+      switch (arg) {
+         case REGULAR:
+            this._price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            this._price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            this._price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
+   }
 }
